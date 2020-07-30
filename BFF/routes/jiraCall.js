9c93fe09150982ed112f32443f851dec9215eed5
Jonathan Peyper <jpeyper@gmail.com>
2020-07-30 18:20:40 +1000

    Fix api to not return the same results multiple times with multiple calls
    Also made the jira url an environment variable

--- BFF/routes/jiraCall.js
+++ BFF/routes/jiraCall.js
@@ -2,54 +2,54 @@ const express = require("express");
 const router = express.Router();
 const requestPromise = require("request-promise");
 
-const result = [];
-
 router.get("/ticketList", async (req, res) => {
   res.setHeader("Content-Type", "application/json");
 
   try {
-    const response = await fetchData("https://jira-uat.auiag.corp/rest/api/2/search?jql=PROJECT+%3D+%22Self+Service+Journey%22+AND+type+%3D+Story+AND++statusCategory+%21%3D+Done&maxResults=60");
+    const response = await fetchData(
+      `${process.env.API_JIRA_HOST}/rest/api/2/search?jql=PROJECT+%3D+%22Self+Service+Journey%22+AND+type+%3D+Story+AND++statusCategory+%21%3D+Done&maxResults=60`
+    );
 
-    await normaliseList(response);
+    const result = await normaliseList(response);
 
     res.json(result);
   } catch (e) {
-    console.error(`Jira call Failed: ${err}`);
-    res.json(err);
+    console.error(`Jira call Failed: ${e}`);
+    res.json(e);
   } finally {
     res.end();
   }
 });
 
 const normaliseIssue = async (issueId) => {
-  const issue = await fetchData(`https://jira-uat.auiag.corp/rest/api/latest/issue/${issueId}`)
+  const issue = await fetchData(
+    `${process.env.API_JIRA_HOST}/rest/api/latest/issue/${issueId}`
+  );
 
-  const issuelinks = (issue.fields.issuelinks || [])
-    .filter(issuelink => issuelink.type.id === "10301")
+  const issuelinks = (issue.fields.issuelinks || []).filter(
+    (issuelink) => issuelink.type.id === "10301"
+  );
 
-  if(issuelinks.length) {
-    result.push({
-      key: issue.key,
-      project: {
-        key: issue.fields.project.key,
-        name: issue.fields.project.name
-      },
-      epic: issue.fields.customfield_12100,
-      points: issue.fields.customfield_10002,
-      estimatedDurationDays: 14,
-      priority: issue.fields.priority.name,
-      dependencies: issuelinks.map(issuelink => issuelink.inwardIssue.key)
-    })
-  }
-
-  return result;
-}
+  return {
+    key: issue.key,
+    project: {
+      key: issue.fields.project.key,
+      name: issue.fields.project.name,
+    },
+    epic: issue.fields.customfield_12100,
+    points: issue.fields.customfield_10002,
+    estimatedDurationDays: 14,
+    priority: issue.fields.priority.name,
+    dependencies: issuelinks.map((issuelink) => issuelink.inwardIssue.key),
+  };
+};
 
 const normaliseList = async (data) => {
-  let issueIds = data.issues.map(issue => issue.id);
+  const issues = data.issues.map((issue) => normaliseIssue(issue.id));
 
-  return await issueIds.forEach(issueId => normaliseIssue(issueId))
-}
+  return await Promise.all(issues);
+};
+
 const fetchData = async (url) => {
   // can await other promises here and use loops or whatever.
 
