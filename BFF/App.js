2d2e7df126fe81fc9a775df32e5f1b7cc473940c
Joseph <joseph.garner@iag.com.au>
2020-07-30 13:53:05 +1000

    Fixed auth issue because I'm dumb

--- BFF/App.js
+++ BFF/App.js
@@ -5,8 +5,8 @@ const morgan = require("morgan");
 const router = require('./routes/jiraCall.js')
 const dotenv = require('dotenv')
 
-dotenv.config()
 app.use(morgan("short"));
+dotenv.config()
 
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
--- BFF/routes/jiraCall.js
+++ BFF/routes/jiraCall.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const requestPromise = require("request-promise");
 
 router.get("/ticketList", async (req, res) => {
+
 	let ticketList = {};
   res.setHeader("Content-Type", "application/json");
   await requestPromise
@@ -11,7 +12,7 @@ router.get("/ticketList", async (req, res) => {
         "https://jira-uat.auiag.corp/rest/api/2/search?jql=PROJECT+%3D+%22Self+Service+Journey%22+AND+type+%3D+Story+AND++statusCategory+%21%3D+Done&maxResults=60",
       headers: {
         Authorization:
-          "Basic " + Buffer.from("[username]:[password]").toString("base64"),
+          "Basic " + Buffer.from(process.env.API_USERNAME + ":" + process.env.API_SECRET).toString("base64"),
       },
     })
     .then((response) => {
