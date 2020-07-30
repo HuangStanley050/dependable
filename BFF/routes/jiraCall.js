bb9ef3afe85c90df936ec6659b9e8a04776168ed
Joseph <joseph.garner@iag.com.au>
2020-07-30 12:28:53 +1000

    Authentication working on API call, However receiving Cert Error

--- BFF/routes/jiraCall.js
+++ BFF/routes/jiraCall.js
@@ -11,14 +11,15 @@ router.get("/ticketList", async (req, res) => {
         "https://jira.iag.com.au/rest/api/2/search?jql=PROJECT+%3D+%22Self+Service+Journey%22+AND+type+%3D+Story+AND++statusCategory+%21%3D+Done&maxResults=60",
       headers: {
         Authorization:
-          "Basic " + btoa(process.env.USER_NAME + ":" + process.env.SECRET),
+          "Basic " + Buffer.from(process.env.USER_NAME + ":" + process.env.SECRET).toString("base64"),
       },
     })
     .then((response) => {
-      ticketList = JSON.parse(response.issues);
+    //   ticketList = JSON.parse(response);
+        ticketList = response
     })
     .catch(function (err) {
-      console.log(`Jira call Failed: ${err.statusCode}`);
+      console.log(`Jira call Failed: ${err}`);
       ticketList = err.statusCode;
     });
   res.json(ticketList);
