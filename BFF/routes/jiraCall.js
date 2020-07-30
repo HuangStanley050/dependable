e6f3c26de74e2ae7581cd91bf10fdd4c663c0b70
Joseph <joseph.garner@iag.com.au>
2020-07-30 13:41:45 +1000

    Updated API address

--- BFF/routes/jiraCall.js
+++ BFF/routes/jiraCall.js
@@ -3,20 +3,19 @@ const router = express.Router();
 const requestPromise = require("request-promise");
 
 router.get("/ticketList", async (req, res) => {
-  let ticketList = {};
+	let ticketList = {};
   res.setHeader("Content-Type", "application/json");
   await requestPromise
     .get({
       url:
-        "https://jira.iag.com.au/rest/api/2/search?jql=PROJECT+%3D+%22Self+Service+Journey%22+AND+type+%3D+Story+AND++statusCategory+%21%3D+Done&maxResults=60",
+        "https://jira-uat.auiag.corp/rest/api/2/search?jql=PROJECT+%3D+%22Self+Service+Journey%22+AND+type+%3D+Story+AND++statusCategory+%21%3D+Done&maxResults=60",
       headers: {
         Authorization:
-          "Basic " + Buffer.from(process.env.USER_NAME + ":" + process.env.SECRET).toString("base64"),
+          "Basic " + Buffer.from("[username]:[password]").toString("base64"),
       },
     })
     .then((response) => {
-    //   ticketList = JSON.parse(response);
-        ticketList = response
+      ticketList = JSON.parse(response);
     })
     .catch(function (err) {
       console.log(`Jira call Failed: ${err}`);
