ae5860a4164f79186795116cd21e5293b271cf1b
Jonathan Peyper <jpeyper@gmail.com>
2020-07-30 14:52:38 +1000

    Add async handler for the requests

--- BFF/routes/jiraCall.js
+++ BFF/routes/jiraCall.js
@@ -4,21 +4,12 @@ const requestPromise = require("request-promise");
 
 router.get("/ticketList", (req, res) => {
   res.setHeader("Content-Type", "application/json");
-  requestPromise
-    .get({
-      url:
-        "https://jira-uat.auiag.corp/rest/api/2/search?jql=PROJECT+%3D+%22Self+Service+Journey%22+AND+type+%3D+Story+AND++statusCategory+%21%3D+Done&maxResults=60",
-      headers: {
-        Authorization:
-          "Basic " + Buffer.from(process.env.API_USERNAME + ":" + process.env.API_SECRET).toString("base64"),
-      },
-      json: true
-    })
+  handleRequest()
     .then((response) => {
       res.json(response);
     })
     .catch(function (err) {
-      console.log(`Jira call Failed: ${err}`);
+      console.error(`Jira call Failed: ${err}`);
       res.json(err);
     })
     .finally(() => {
@@ -26,4 +17,23 @@ router.get("/ticketList", (req, res) => {
     });
 });
 
-module.exports = router
+const handleRequest = async () => {
+  const response = await requestPromise.get({
+    url:
+      "https://jira-uat.auiag.corp/rest/api/2/search?jql=PROJECT+%3D+%22Self+Service+Journey%22+AND+type+%3D+Story+AND++statusCategory+%21%3D+Done&maxResults=60",
+    headers: {
+      Authorization:
+        "Basic " +
+        Buffer.from(
+          process.env.API_USERNAME + ":" + process.env.API_SECRET
+        ).toString("base64"),
+    },
+    json: true,
+  });
+
+  // can await other promises here and use loops or whatever.
+
+  return response;
+};
+
+module.exports = router;
