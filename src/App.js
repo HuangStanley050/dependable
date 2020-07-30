c9ddddfa43051b815e56016982ccf7e20e2511d6
Joseph <joseph.garner@iag.com.au>
2020-07-30 16:33:53 +1000

    Updated Gantt Chart with mock Data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Chart } from "react-google-charts";
 import Gantt from "./components/ganttChart"
-import ApiCall from "./components/apiCall"
 
 function App() {
   return (
-    <div>
-      {/* <Gantt/> */}
-      <ApiCall/>
+    <div className="graph">
+      <Gantt/>
     </div>
   );
 }
