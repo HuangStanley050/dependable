449cca98a194c8f88fd93d64c1f0f67194696300
Maysam Tayyeb <maysam.tayyeb@iag.com.au>
2020-07-30 17:28:53 +1000

    Round one of fetching data

--- BFF/routes/jiraCall.js
+++ BFF/routes/jiraCall.js
@@ -2,12 +2,17 @@ const express = require("express");
 const router = express.Router();
 const requestPromise = require("request-promise");
 
+const result = [];
+
 router.get("/ticketList", async (req, res) => {
   res.setHeader("Content-Type", "application/json");
 
   try {
     const response = await fetchData("https://jira-uat.auiag.corp/rest/api/2/search?jql=PROJECT+%3D+%22Self+Service+Journey%22+AND+type+%3D+Story+AND++statusCategory+%21%3D+Done&maxResults=60");
-    res.json(response);
+
+    await normaliseList(response);
+
+    res.json(result);
   } catch (e) {
     console.error(`Jira call Failed: ${err}`);
     res.json(err);
@@ -16,6 +21,35 @@ router.get("/ticketList", async (req, res) => {
   }
 });
 
+const normaliseIssue = async (issueId) => {
+  const issue = await fetchData(`https://jira-uat.auiag.corp/rest/api/latest/issue/${issueId}`)
+
+  const issuelinks = (issue.fields.issuelinks || [])
+    .filter(issuelink => issuelink.type.id === "10301")
+
+  if(issuelinks.length) {
+    result.push({
+      key: issue.key,
+      project: {
+        key: issue.fields.project.key,
+        name: issue.fields.project.name
+      },
+      epic: issue.fields.customfield_12100,
+      points: issue.fields.customfield_10002,
+      estimatedDurationDays: 14,
+      priority: issue.fields.priority.name,
+      dependencies: issuelinks.map(issuelink => issuelink.inwardIssue.key)
+    })
+  }
+
+  return result;
+}
+
+const normaliseList = async (data) => {
+  let issueIds = data.issues.map(issue => issue.id);
+
+  return await issueIds.forEach(issueId => normaliseIssue(issueId))
+}
 const fetchData = async (url) => {
   // can await other promises here and use loops or whatever.
 
