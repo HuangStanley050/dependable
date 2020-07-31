7ecbcaf951c7ffc65b7796a121944a3b54a16cca
Maysam Tayyeb <maysam.tayyeb@iag.com.au>
2020-07-31 11:27:08 +1000

    Fetch the dependencies

--- BFF/routes/jiraCall.js
+++ BFF/routes/jiraCall.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const requestPromise = require("request-promise");
 
-// const cache = {}
+const cache = {}
 
 router.get("/ticketList", async (req, res) => {
   res.setHeader("Content-Type", "application/json");
@@ -12,74 +12,90 @@ router.get("/ticketList", async (req, res) => {
       `${process.env.API_JIRA_HOST}/rest/api/2/search?jql=PROJECT+%3D+%22Self+Service+Journey%22+AND+type+%3D+Story+AND++statusCategory+%21%3D+Done&maxResults=60`
     );
 
-    const result = await normaliseList(response);
+    const result = await normaliseResponse(response);
 
     res.json(result);
   } catch (e) {
-    console.error(`Jira call Failed: ${e}`);
+    console.error("Jira call Failed: ", e);
     res.json(e);
   } finally {
     res.end();
   }
 });
 
-const normaliseIssue = async (issueId) => {
-  // if(cache[issueId]) {
-  //   return cache[issueId]
-  // }
+const normaliseIssue = async (issueId, results) => {
+  if (cache[issueId]) {
+    return cache[issueId]
+  }
 
-  const issue = await fetchData(
-    `${process.env.API_JIRA_HOST}/rest/api/latest/issue/${issueId}`
-  );
+  try {
+    const issue = await fetchData(
+      `${process.env.API_JIRA_HOST}/rest/api/latest/issue/${issueId}`
+    );
 
-  // cache[issueId] = issue
+    const issuelinks = (issue.fields.issuelinks || []).filter(
+      (issuelink) => {
+        return issuelink.type.id === "10301" && issuelink.inwardIssue
+      }
+    );
 
-  const issuelinks = (issue.fields.issuelinks || []).filter(
-    (issuelink) => issuelink.type.id === "10301"
-  );
+    // if (issuelinks.length) {
+    //   await normaliseIssues(issuelinks.map((issuelink) => issuelink.inwardIssue.id), results)
+    // }
 
-  // for each dependency {
-  //   await normaliseIssue(dependencyID)
-  // }
+    cache[issueId] = {
+      key: issue.key,
+      project: {
+        key: issue.fields.project.key,
+        name: issue.fields.project.name,
+      },
+      epic: issue.fields.customfield_12100,
+      points: issue.fields.customfield_10002,
+      estimatedDurationDays: 14,
+      priority: issue.fields.priority.name,
+      dependencyKeys: issuelinks.map((issuelink) => {
+        return issuelink.inwardIssue.id
+      }),
+      dependencies: issuelinks.map((issuelink) => issuelink.inwardIssue.key),
+    }
 
-//   return [{
-//     key: issue.key,
-//     project: {
-//       key: issue.fields.project.key,
-//       name: issue.fields.project.name,
-//     },
-//     epic: issue.fields.customfield_12100,
-//     points: issue.fields.customfield_10002,
-//     estimatedDurationDays: 14,
-//     priority: issue.fields.priority.name,
-//     dependencies: issuelinks.map((issuelink) => issuelink.inwardIssue.key),
-//   },
-//   ...dependencies
-// ];
-
-  return {
-    key: issue.key,
-    project: {
-      key: issue.fields.project.key,
-      name: issue.fields.project.name,
-    },
-    epic: issue.fields.customfield_12100,
-    points: issue.fields.customfield_10002,
-    estimatedDurationDays: 14,
-    priority: issue.fields.priority.name,
-    dependencies: issuelinks.map((issuelink) => issuelink.inwardIssue.key),
+    return cache[issueId]
+  } catch (e) {
+    console.log("normaliseIssue failed: ", e)
   }
 };
 
-const normaliseList = async (data) => {
-  const issues = data.issues.map((issue) => normaliseIssue(issue.id));
+const normaliseIssues = async (issueIds, results) => {
+  try {
+    const issues = issueIds.map((issueId) => normaliseIssue(issueId, results));
+    const normalisedIssues = await Promise.all(issues)
 
-  const results = await Promise.all(issues);
+    normalisedIssues.forEach(normalisedIssue => {
+      results.push(normalisedIssue)
+    })
 
-  // flatten results into non nested array
+    const deps = normalisedIssues.flatMap(normalisedIssue => normalisedIssue.dependencyKeys)
+
+    if (deps.length) {
+      await normaliseIssues(deps, results)
+    }
+  } catch (e) {
+    console.log("normaliseIssues failed: ", e)
+  }
+};
+
+const normaliseResponse = async (data) => {
+  const issueIds = data.issues.map((issue) => issue.id)
+  const results = [];
+
+  try {
+    await normaliseIssues(issueIds, results)
+  } catch (e) {
+    console.log("normaliseResponse failed: ", e)
+  }
 
   return results
-};
+}
 
 const fetchData = async (url) => {
   // can await other promises here and use loops or whatever.
